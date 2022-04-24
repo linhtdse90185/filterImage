3ba42e4cf2e6adc4bb34d559dda19156e02ef305
Linh <tdlinh89@gmail.com>
2022-04-24 19:32:08 +0700

    commit source code

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+import express, { Request, Response, NextFunction, Errback } from 'express';
+import bodyParser from 'body-parser';
+import fs from "fs";
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port:number|string = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // path save image 
+  const dir_save:string = "/util/tmp/";
+
+  // IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+  app.get( "/filteredimage", async ( req: Request, res: Response, next: NextFunction ) => {
+    let { image_url } = req.query;
+    // check Caption is valid
+    if (!image_url) {
+      return next('Bad request. Please add query param image_url.');
+    }
+    try {
+      let absolutePath: string = await filterImageFromURL(image_url) as string;
+      res.status(200).sendFile(absolutePath);
+      fs.readdir(__dirname + dir_save, (err: Error, files: string[]) => {
+        const fileList: string[] = [];
+        files.forEach((file: string) => {
+          fileList.push(__dirname + dir_save + file);
+        });
+        deleteLocalFiles(fileList);
+      });
+      return;
+    } catch (e) {
+      return next('It was unable to process.' + e);
+    }
+   
+  } );
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req: Request, res: Response ) => {
+    return res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
